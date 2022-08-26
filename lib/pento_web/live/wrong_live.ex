e2a8c765cb91e01c2b8c6db26f6fb44e12968a3a
r4z4 <85771218+r4z4@users.noreply.github.com>
2022-08-25 20:25:39 -0500

    lib and /web

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,54 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+  alias Pento.Accounts
+
+  def mount(_params, session, socket) do
+    {
+      :ok,
+      assign(
+        socket,
+        score: 0,
+        message: "Guess a number.",
+        time: time(),
+        user: Pento.Accounts.get_user_by_session_token(session["user_token"]),
+        session_id: session["live_socket_id"]
+      )
+    }
+  end
+
+  def handle_event("guess", %{"number" => guess}=data, socket) do
+    IO.inspect data
+    message = "Your guess: #{guess}. Wrong. Guess again. "
+    score = socket.assigns.score - 1
+    {
+      :noreply,
+      assign(
+        socket,
+        message: message,
+        score: score,
+        time: time())}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Your score: <%= @score %></h1>
+    <h2>
+      <%= @message %>
+      It's <%= @time %>
+    </h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <a href="#" phx-click="guess" phx-value-number="<%= n %>"><%= n %></a>
+      <% end %>
+    </h2>
+    <pre>
+      <%= @user.email %>
+      <%= @session_id %>
+    </pre>
+    """
+  end
+
+  def time() do
+    DateTime.utc_now |> to_string
+  end
+end
